b0d6b34351fe4305d78a0eddfb7b3e85f6aad369
Szymon Gajda <mrucznix@gmail.com>
2016-03-28 11:54:01 +0200

    Initial Commit

---
+++ source/Mrucznik/src/GameMode.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using SampSharp.GameMode;
+using SampSharp.GameMode.Controllers;
+using SampSharp.GameMode.Definitions;
+using SampSharp.GameMode.SAMP.Commands;
+using SampSharp.GameMode.World;
+using SampSharp.Streamer;
+
+namespace Mrucznik
+{
+    public class GameMode : BaseMode
+    {
+        #region Overrides of BaseMode
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            Console.WriteLine("\n----------------------------------");
+            Console.WriteLine("M | --- Mrucznik Role Play --- | M");
+            Console.WriteLine("R | ---        ****        --- | R");
+            Console.WriteLine("U | ---        v3.0        --- | U");
+            Console.WriteLine("C | ---        ****        --- | C");
+            Console.WriteLine("Z | ---    by Mrucznik     --- | Z");
+            Console.WriteLine("N | ---                    --- | N");
+            Console.WriteLine("I | ---       /\\_/\\        --- | I");
+            Console.WriteLine("K | ---   ===( *.* )===    --- | K");
+            Console.WriteLine("  | ---       \\_^_/        --- |  ");
+            Console.WriteLine("R | ---         |          --- | R");
+            Console.WriteLine("P | ---         O          --- | P");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Wersja: " + Assembly.GetExecutingAssembly().GetName().Version);
+            Console.WriteLine("\n");
+
+            //Ustawienia SAMP'a
+            SetGameModeText("Mrucznik-RP " + Assembly.GetExecutingAssembly().GetName().Version);
+            AllowInteriorWeapons(true);
+            ShowPlayerMarkers(PlayerMarkersMode.Off);
+            DisableInteriorEnterExits();
+            EnableStuntBonusForAll(false);
+            ManualVehicleEngineAndLights();
+            ShowNameTags(true);
+            SetNameTagDrawDistance(20.0f);
+
+            //TODO: Pogoda
+            SetWeather(2);
+            SetWorldTime(11);
+
+            //Klasy:
+            AddPlayerClass(1, new Vector3(1759.0189f, -1898.1260f, 13.5622f), 266.4503f);
+            AddPlayerClass(2, new Vector3(1759.0189f, -1898.1260f, 13.5622f), 266.4503f);
+            AddPlayerClass(3, new Vector3(1759.0189f, -1898.1260f, 13.5622f), 266.4503f);
+            AddPlayerClass(4, new Vector3(1759.0189f, -1898.1260f, 13.5622f), 266.4503f);
+
+            base.OnInitialized(e);
+        }
+
+        protected override void LoadControllers(ControllerCollection controllers)
+        {
+            base.LoadControllers(controllers); //first
+
+            Streamer.Load(this, controllers);
+        }
+
+        [Command("kill")]
+        public static void KillMe(GtaPlayer player)
+        {
+            player.Health = 0.0f;
+        }
+
+        #endregion
+    }
+}
---
+++ source/Mrucznik/src/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SampSharp.GameMode.World;
+
+namespace Mrucznik
+{
+    public class Player : GtaPlayer
+    {
+        public Player(int id) : base(id)
+        {
+        }
+
+        public override void OnConnected(EventArgs e)
+        {
+            base.OnConnected(e);
+
+            Console.WriteLine(Name + " is connected.");
+
+            SendClientMessage("Welcome to {88AA88}M{FFFFFF}rucznik {88AA88}R{FFFFFF}ole{88AA88}P{FFFFFF}lay");
+            GameText("~w~Mrucznik Role Play", 4000, 4);
+        }
+    }
+}
