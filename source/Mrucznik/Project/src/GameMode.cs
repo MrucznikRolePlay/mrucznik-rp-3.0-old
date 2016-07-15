be57ebe647ce1d2d17b8cd5a402d0fb4b22360f9
Mrucznik <mrucznix@gmail.com>
2016-07-15 13:32:32 +0200

    Przebudowa projektu

---
+++ source/Mrucznik/Project/src/GameMode.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Reflection;
+using Mrucznik.Controllers;
+using SampSharp.GameMode;
+using SampSharp.GameMode.Controllers;
+using SampSharp.GameMode.Definitions;
+using SampSharp.GameMode.Events;
+//using SampSharp.Streamer;
+
+namespace Mrucznik
+{
+    public class GameMode : BaseMode
+    {
+        #region Overrides of BaseMode
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            string version = "Mrucznik - RP " + Assembly.GetExecutingAssembly().GetName().Version + " - alpha";
+            Console.WriteLine("\n----------------------------------");
+            Console.WriteLine("M | --- Mrucznik Role Play --- | M");
+            Console.WriteLine("R | ---        ****        --- | R");
+            Console.WriteLine("U | ---        v3.0        --- | U");
+            Console.WriteLine("C | ---        ****        --- | C");
+            Console.WriteLine("Z | ---    by Mrucznik     --- | Z");
+            Console.WriteLine("N | ---                    --- | N");
+            Console.WriteLine("I | ---       /\\_/\\        --- | I");
+            Console.WriteLine("K | ---   ===( *.* )===    --- | K");
+            Console.WriteLine("  | ---       \\_^_/        --- |  ");
+            Console.WriteLine("R | ---         |          --- | R");
+            Console.WriteLine("P | ---         O          --- | P");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Wersja: {0}", version);
+
+            //Ustawienia SAMP'a
+            SetGameModeText(version);
+            AllowInteriorWeapons(true);
+            ShowPlayerMarkers(PlayerMarkersMode.Off);
+            DisableInteriorEnterExits();
+            EnableStuntBonusForAll(false);
+            ManualVehicleEngineAndLights();
+            ShowNameTags(true);
+            SetNameTagDrawDistance(20.0f);
+
+            //TODO: Pogoda - system pogody
+            SetWeather(2);
+            SetWorldTime(11);
+
+            //Klasy:
+            AddPlayerClass(1, new Vector3(1759.0189f, -1898.1260f, 13.5622f), 266.4503f);
+            AddPlayerClass(2, new Vector3(1759.0189f, -1898.1260f, 13.5622f), 266.4503f);
+            AddPlayerClass(3, new Vector3(1759.0189f, -1898.1260f, 13.5622f), 266.4503f);
+            AddPlayerClass(4, new Vector3(1759.0189f, -1898.1260f, 13.5622f), 266.4503f);
+
+            base.OnInitialized(e);
+        }
+
+        protected override void OnExited(EventArgs e)
+        {
+            base.OnExited(e);
+
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("---------- GAMEMODE OFF ----------");
+            Console.WriteLine("----------------------------------");
+        }
+
+        protected override void OnIncomingConnection(ConnectionEventArgs e)
+        {
+            base.OnIncomingConnection(e);
+        }
+
+        protected override void OnRconCommand(RconEventArgs e)
+        {
+            base.OnRconCommand(e);
+        }
+
+        protected override void OnRconLoginAttempt(RconLoginAttemptEventArgs e)
+        {
+            base.OnRconLoginAttempt(e);
+        }
+
+        protected override void OnTick(EventArgs e)
+        {
+            base.OnTick(e);
+        }
+
+        protected override void LoadControllers(ControllerCollection controllers)
+        {
+            base.LoadControllers(controllers); //first
+
+            controllers.Remove<GtaPlayerController>();
+            controllers.Remove<GtaVehicleController>();
+            controllers.Add(new PlayerController());
+            controllers.Add(new VehicleController());
+
+            //Streamer.Load(this, controllers);
+        }
+        
+        #endregion
+    }
+}
---
+++ source/Mrucznik/Project/src/ProgressBar.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using SampSharp.GameMode;
+using SampSharp.GameMode.Display;
+using SampSharp.GameMode.Pools;
+using SampSharp.GameMode.SAMP;
+using SampSharp.GameMode.World;
+using SampSharp.GameMode.Tools;
+
+namespace Mrucznik
+{
+    class ProgressBar : Pool<ProgressBar>
+    {
+        private readonly PlayerTextDraw _back;
+        private readonly PlayerTextDraw _fill;
+        private readonly PlayerTextDraw _main;
+        private float _value;
+        private float _max;
+
+        public ProgressBar(GtaPlayer player, float x, float y, float value, Color color = default(Color),
+            float width = 55.5f, float height = 3.2f,
+            float max = 100.0f, ProgressBarDirection direction = ProgressBarDirection.Right)
+        {
+            X = x;
+            Y = y;
+            _value = value;
+            Width = width;
+            Height = height;
+            Color = color;
+            _max = max;
+            Direction = direction;
+
+            if (player == null) throw new ArgumentNullException("player");
+
+            switch (direction)
+            {
+                case ProgressBarDirection.Right:
+                    _back = new PlayerTextDraw(player, new Vector2(x, y), "_")
+                    {
+                        UseBox = true,
+                        Width = x + width - 4.0f,
+                        Height = 0.0f,
+                        LetterWidth = 1.0f,
+                        LetterHeight = height / 10,
+                        BoxColor = 0x00000000 | (color & 0x000000FF)
+                    };
+
+                    _fill = new PlayerTextDraw(player, new Vector2(x + 1.2f, y + 2.15f), "_")
+                    {
+                        UseBox = true,
+                        Width = x + width - 5.5f,
+                        Height = 0.0f,
+                        LetterWidth = 1.0f,
+                        LetterHeight = height / 10 - 0.35f,
+                        BoxColor = (int)((color & 0xFFFFFF00) | (0x66 & ((color & 0x000000FF) / 2)))
+                    };
+
+                    _main = new PlayerTextDraw(player, new Vector2(x + 1.2f, y + 2.15f), "_")
+                    {
+                        UseBox = true,
+                        Width = CalculatePercentage(),
+                        Height = 0.0f,
+                        LetterWidth = 1.0f,
+                        LetterHeight = height / 10 - 0.35f,
+                        BoxColor = color
+                    };
+                    break;
+                case ProgressBarDirection.Left:
+                    _back = new PlayerTextDraw(player, new Vector2(x, y), "_")
+                    {
+                        UseBox = true,
+                        Width = x - width - 4.0f,
+                        Height = 0.0f,
+                        LetterWidth = 1.0f,
+                        LetterHeight = height / 10,
+                        BoxColor = 0x00000000 | (color & 0x000000FF)
+                    };
+
+                    _fill = new PlayerTextDraw(player, new Vector2(x - 1.2f, y + 2.15f), "_")
+                    {
+                        UseBox = true,
+                        Width = x - width - 2.5f,
+                        Height = 0.0f,
+                        LetterWidth = 1.0f,
+                        LetterHeight = height / 10 - 0.35f,
+                        BoxColor = (int)((color & 0xFFFFFF00) | (0x66 & ((color & 0x000000FF) / 2)))
+                    };
+
+                    _main = new PlayerTextDraw(player, new Vector2(x - 1.2f, y + 2.15f), "_")
+                    {
+                        UseBox = true,
+                        Width = CalculatePercentage(),
+                        Height = 0.0f,
+                        LetterWidth = 1.0f,
+                        LetterHeight = height / 10 - 0.35f,
+                        BoxColor = color
+                    };
+                    break;
+                case ProgressBarDirection.Up:
+                    _back = new PlayerTextDraw(player, new Vector2(x, y), "_")
+                    {
+                        UseBox = true,
+                        Width = x - width - 4.0f,
+                        Height = 0.0f,
+                        LetterWidth = 1.0f,
+                        LetterHeight = -((height / 10) * 1.02f) - 0.35f,
+                        BoxColor = 0x00000000 | (color & 0x000000FF)
+                    };
+
+                    _fill = new PlayerTextDraw(player, new Vector2(x - 1.2f, y - 1.0f), "_")
+                    {
+                        UseBox = true,
+                        Width = x - width - 2.5f,
+                        Height = 0.0f,
+                        LetterWidth = 1.0f,
+                        LetterHeight = -(height / 10.0f) * 1.02f,
+                        BoxColor = (int)((color & 0xFFFFFF00) | (0x66 & ((color & 0x000000FF) / 2)))
+                    };
+
+                    _main = new PlayerTextDraw(player, new Vector2(x - 1.2f, y - 1.0f), "_")
+                    {
+                        UseBox = true,
+                        Width = x - width - 2.5f,
+                        Height = 0.0f,
+                        LetterWidth = 0.0f,
+                        LetterHeight = CalculatePercentage(),
+                        BoxColor = color
+                    };
+                    break;
+
+                case ProgressBarDirection.Down:
+                    _back = new PlayerTextDraw(player, new Vector2(x, y), "_")
+                    {
+                        UseBox = true,
+                        Width = x - width - 4.0f,
+                        Height = 0.0f,
+                        LetterWidth = 1.0f,
+                        LetterHeight = (height / 10) - 0.35f,
+                        BoxColor = 0x00000000 | (color & 0x000000FF)
+                    };
+
+                    _fill = new PlayerTextDraw(player, new Vector2(x - 1.2f, y + 1.0f), "_")
+                    {
+                        UseBox = true,
+                        Width = x - width - 2.5f,
+                        Height = 0.0f,
+                        LetterWidth = 1.0f,
+                        LetterHeight = (height / 10.0f) - 0.55f,
+                        BoxColor = (int)((color & 0xFFFFFF00) | (0x66 & ((color & 0x000000FF) / 2)))
+                    };
+
+                    _main = new PlayerTextDraw(player, new Vector2(x - 1.2f, y + 1.0f), "_")
+                    {
+                        UseBox = true,
+                        Width = x - width - 2.5f,
+                        Height = 0.0f,
+                        LetterWidth = 0.0f,
+                        LetterHeight = CalculatePercentage(),
+                        BoxColor = color
+                    };
+                    break;
+            }
+        }
+
+        public float X { get; private set; }
+        public float Y { get; private set; }
+        public float Width { get; private set; }
+        public float Height { get; private set; }
+        public Color Color { get; private set; }
+
+        public float Max
+        {
+            get { return _max; }
+            private set
+            {
+                _max = value;
+                Redraw();
+            }
+        }
+
+        public float Value
+        {
+            get { return _value; }
+            set
+            {
+                _value = value;
+                Redraw();
+            }
+        }
+
+        public ProgressBarDirection Direction { get; private set; }
+
+        private float CalculatePercentage()
+        {
+            float result = 0;
+
+            switch (Direction)
+            {
+                case ProgressBarDirection.Right:
+                    result = ((X - 3.0f) + (((((X - 2.0f) + Width) - X) / Max) * Value));
+                    break;
+                case ProgressBarDirection.Left:
+                    result = ((X - 1.0f) - (((((X + 2.0f) - Width) - X) / Max) * -Value)) - 4.0f;
+                    break;
+                case ProgressBarDirection.Up:
+                    result = -((((((Height / 10.0f) - 0.45f) * 1.02f) / Max) * Value) + 0.55f);
+                    break;
+                case ProgressBarDirection.Down:
+                    result = ((((((Height / 10.0f) - 0.45f) * 1.02f) / Max) * Value) - 0.55f);
+                    break;
+            }
+
+            return result;
+        }
+
+        private void Redraw()
+        {
+            if (_max < 0.1f) _max = 0.1f;
+            _value = _value < 0 ? 0 : (_value > Max ? Max : _value);
+
+            _main.UseBox = _value > 0.0f;
+
+            switch (Direction)
+            {
+                case ProgressBarDirection.Right:
+                case ProgressBarDirection.Left:
+                    {
+                        _main.Width = CalculatePercentage();
+                    }
+                    break;
+                case ProgressBarDirection.Up:
+                case ProgressBarDirection.Down:
+                    {
+                        _main.LetterHeight = CalculatePercentage();
+                    }
+                    break;
+            }
+
+            Show();
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!IsDisposed) Hide();
+
+            _back?.Dispose();
+            _fill?.Dispose();
+            _main?.Dispose();
+        }
+
+        public void Show()
+        {
+            //CheckDisposure();
+            AssertNotDisposed();
+
+            _back.Show();
+            _fill.Show();
+            _main.Show();
+        }
+
+        public void Hide()
+        {
+            //CheckDisposure();
+            AssertNotDisposed();
+
+            _back.Hide();
+            _fill.Hide();
+            _main.Hide();
+        }
+
+    }
+
+    public enum ProgressBarDirection
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+}
